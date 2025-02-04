77357ccaf9e1a8b4bbb21e34f1d25ba262daa9cb
gams2206 <guilhermesabino96@gmail.com>
2025-02-04 20:11:39 -0300

    too many changes

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -4,7 +4,7 @@ import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default ({title, items}) => {
-    const [scrollX, setScrollX] = useState(-400);
+    const [scrollX, setScrollX] = useState(-7);
 
     const handleLeftArrow = () => {
         let x = scrollX + Math.round(window.innerWidth / 2);
